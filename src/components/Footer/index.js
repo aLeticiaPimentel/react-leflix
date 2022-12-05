324ac8f425c1696c37efbd19b855a9a0c8fa615e
Leticia Pimentel <le_pds100@hotmail.com>
2022-12-05 19:34:36 -0300

    Setup projeto inicial

---
+++ src/components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
+
+function Footer() {
+  return (
+    <FooterBase>  
+        <img src={Logo} alt="Logo Leflix" />
+      <p>
+        Orgulhosamente criado durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
